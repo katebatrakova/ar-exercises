247ec10c0fbeb3a7722c7651fe907093892b9c96
Kateryna <katerynabatrakova@gmail.com>
2020-08-17 22:38:57 +0000

    Complete Ex.2: assign first and second stores to instance variables, update revenue of store1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.first
+@store2 = Store.find_by(id: 2)
+# change the revenue of the first store 
+@store1.annual_revenue = "440000"
